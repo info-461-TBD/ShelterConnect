4958603d44f18d2b619c70d036bbb5c4df25589a
Ryker Schwartzenberger <rykerls@uw.edu>
2018-05-15 17:04:42 -0700

    Firebase auth working for test account

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from "react-router";
 import Routes from "./routes";
+import firebase from "firebase";
+import * as firebase_config from "./firebase_config.js";
 
 import "./index.css";
 
 const outlet = document.getElementById("root");
+
+firebase.initializeApp(firebase_config.config);
+
+/* This code just for testing the connection to Firebase. */
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log(user.displayName);
+        console.log(user.email);
+        console.log(user.emailVerified);
+        console.log(user.uid);
+    }
+});
+
+firebase.auth().signInWithEmailAndPassword('rykerls@uw.edu', 'test00').catch(function(error) {
+// Handle Errors here.
+console.log(error.code);
+console.log(error.message);
+
+});
+
 ReactDOM.render(<Router history={browserHistory} routes={Routes} />, outlet);
