9569f01bab872b23a3cb4c5c5ff7928c56b34f37
Joseph Zhang <cjzhang@uw.edu>
2018-05-22 16:47:20 -0700

    queryByValue function added to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,23 @@ import logo from "./logo.svg";
 import "./style.css";
 
 export default class Home extends Component {
+	// Import Admin SDK
+	var admin = require("firebase-admin");
+
+	// Get a database reference to our posts
+	var db = admin.database();
+	var ref = db.ref("requests");
+
+	// query data ordered by a given child key
+	queryByValue(child) {
+		var jsonData = [];
+		ref.orderByChild(child).on("child_added", function(snapshot) {
+			console.log(snapshot.key + ": " + snapshot.val().child);
+			jsonData.push(snapshot);
+		});
+		return jsonData;
+	}
+	
 	render() {
 		return (
 			<div className={classnames("App", this.props.className)}>
