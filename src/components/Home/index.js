96c181a26c5aaecbc6aa9b6816c54285a94090a5
Joseph Zhang <cjzhang@uw.edu>
2018-05-22 17:11:56 -0700

    fixed firebase ref

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 import logo from "./logo.svg";
 import "./style.css";
 
 export default class Home extends Component {
-	// Import Admin SDK
-	var admin = require("firebase-admin");
-
-	// Get a database reference to our posts
-	var db = admin.database();
-	var ref = db.ref("requests");
+	
 
 	// query data ordered by a given child key
 	queryByValue(child) {
 		var jsonData = [];
-		ref.orderByChild(child).on("child_added", function(snapshot) {
+		firebase.database().ref("requests").orderByChild(child).on("child_added", function(snapshot) {
 			console.log(snapshot.key + ": " + snapshot.val().child);
 			jsonData.push(snapshot);
 		});
