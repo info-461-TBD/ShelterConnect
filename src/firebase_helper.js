17a17e0295bd9b1d5d80756e23ddee7423444842
Ryker Schwartzenberger <rykerls@uw.edu>
2018-05-24 14:34:57 -0700

    moved firebase code over to it's own module. Started working on functionality needed for other components.

---
+++ src/firebase_helper.js
@@ -0,0 +1,81 @@
+import firebase from "firebase";
+import * as firebase_config from "./firebase_config.js";
+
+/* The firebase database object */
+var db;
+
+/* 
+    Initializes the firebase app connection
+    You only need to run this once in components/index.js
+*/
+export function initialize() {
+    try {
+        firebase.initializeApp(firebase_config.config);
+        db = firebase.database();
+    }
+    catch(error) {
+        console.error(error);
+        return false;
+    }
+    return true;
+}
+
+export function signInUser() {
+    firebase.auth().signInWithEmailAndPassword('rykerls@uw.edu', 'test00').catch(function(error) {
+        // Handle Errors here.
+        console.log(error.code);
+        console.log(error.message);
+        return false;
+        });
+
+    /* This code just for testing the connection to Firebase. */
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log(user.displayName);
+            console.log(user.email);
+            console.log(user.emailVerified);
+            console.log(user.uid);
+            console.log(user.photoURL);
+        }
+    });
+
+    return true;
+
+}
+
+export function getRequestList() {
+    
+    var request_list = [];
+
+    var request_ref = db.ref("requests");
+    request_ref.on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var data = childSnapshot.val();
+            var element = {};
+
+            element.key = childSnapshot.key;
+            element.date_added = data.date_added;
+            element.donation_type = data.donation_type;
+            element.request_text = data.request_text;
+            
+            var uid = data.ptr_user_account;
+            element.uid = uid
+            
+            db.ref("users/" + uid).once('value').then(function(snapshot) {
+                var cdata = snapshot.val()
+                element.org_address = cdata.org_address;
+                element.org_description = cdata.org_description;
+                element.org_email = cdata.org_email;
+                element.org_name = cdata.org_name;
+                element.org_phone = cdata.org_phone;
+
+            });
+            request_list.push(element);
+            
+        })
+    }, function(errorObject) {
+        console.log("read failed: " + errorObject.code);
+    });
+    return request_list;
+}
+
--- src/index.js
+++ src/index.js
@@ -2,39 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from "react-router";
 import Routes from "./routes";
-import firebase from "firebase";
-import * as firebase_config from "./firebase_config.js";
+import * as firebase_helper from "./firebase_helper.js";
 
 import "./index.css";
 
 const outlet = document.getElementById("root");
 
-firebase.initializeApp(firebase_config.config);
-
-/* This code just for testing the connection to Firebase. */
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        console.log(user.displayName);
-        console.log(user.email);
-        console.log(user.emailVerified);
-        console.log(user.uid);
+if (firebase_helper.initialize()) {
+    // Signs in user rykerls@uw.edu
+    if (firebase_helper.signInUser()) {
+        console.log(firebase_helper.getRequestList());
     }
-});
-
-firebase.auth().signInWithEmailAndPassword('rykerls@uw.edu', 'test00').catch(function(error) {
-// Handle Errors here.
-console.log(error.code);
-console.log(error.message);
-
-});
-
-const db = firebase.database();
-var request_ref = db.ref("requests");
-
-request_ref.on("value", function(snapshot) {
-    console.log(snapshot.val());
-}, function(errorObject) {
-    console.log("read failed: " + errorObject.code);
-});
+}
 
 ReactDOM.render(<Router history={browserHistory} routes={Routes} />, outlet);
