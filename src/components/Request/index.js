0406c43d42c7c4c33618bbe727fa5dcadc42dada
Simon Bang <bangtsimon@gmail.com>
2018-05-29 17:05:28 -0700

    added some functionality to homepage

--- src/components/Request/index.js
+++ src/components/Request/index.js
@@ -8,27 +8,39 @@ const DEFAULT_IMG = "";
 export default class Request extends React.Component {
     constructor(props) {
         super(props);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.state = {
+            show: false
+        };
     }
 
+    handleClose() {
+        this.setState({ show: false });
+      }
+    
+    handleShow() {
+    this.setState({ show: true });
+    }
     render() {
-        var tel = "tel:" + this.props.tel
-        var email = "mailto:" + this.props.email + "?Subject=Complete Request " + this.props.id
+        var tel = "tel:" + this.props.request.tel
+        var email = "mailto:" + this.props.request.email + "?Subject=Complete Request " + this.props.id
         return (
             <Modal show={this.state.show} onHide={this.handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Request for {this.props.organization}</Modal.Title>
+                    <Modal.Title>Request for {this.props.request.organization}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <img src={this.props.image} alt=""/>
-                    <span>Start Date: {this.props.date}</span>
-                    <span>Date Needed by:{this.props.daterequired}</span>
+                    <img src={this.props.request.image} alt=""/>
+                    <span>Start Date: {this.props.request.date}</span>
+                    <span>Date Needed by:{this.props.request.daterequired}</span>
                     <p>
-                        {this.props.request}  
+                        {this.props.request.details}  
                     </p>
                     <a href={tel}>Call</a>
                     <a href={email}>Send Email</a>
                     <address>
-                        {this.props.address}
+                        {this.props.request.address}
                     </address>
                 </Modal.Body>
             <Modal.Footer>
