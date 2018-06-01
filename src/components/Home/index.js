3049ebd2c485d7d7638834e545e57bc003662018
Simon Bang <bangtsimon@gmail.com>
2018-05-31 17:17:57 -0700

    added request

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, ListGroup, ListGroupItem} from "react-bootstrap";
+import { Grid, ListGroup, ListGroupItem, DropdownButton, ButtonGroup, Button, MenuItem } from "react-bootstrap";
 import { browserHistory } from "react-router";
 import classnames from "classnames";
 
@@ -12,21 +12,30 @@ export default class Home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			requests: undefined,
+			requests: [],
 		}
 	}
 
 	componentWillMount() {
-		this.state.requests = getRequestList();
+		var newRequests = getRequestList();
+		this.setState({requests: newRequests});
 	}
 	handleMove() {
 		browserHistory.push("/signup");
 	}
-  
-	componentDidMount() {
-		this.queryByValue("request_text");
-	}
 
+	handleDate = () => {
+		var newRequests = getRequestList();
+		this.setState({requests: newRequests});
+	}
+	
+	/*
+	handleOrganization = () => {
+		var newRequests = getOrganizationRequests();
+		this.setState({requests: newRequests});
+	}
+	*/
+  
 	// query data ordered by a given child key
 	// child keys: date_added, donation_type, ptr_user_account, request_text
 	queryByValue(child) {
@@ -37,8 +46,6 @@ export default class Home extends Component {
 		});
 		return jsonData;
 	}
-	
-	
 
 	render() {
 		var requests;
@@ -48,12 +55,24 @@ export default class Home extends Component {
 				</Request>
 			</ListGroupItem>
 		);
+		/*organizations = this.state.organizations.map(o =>
+			<MenuItem key={o.id}>
+				{o.name}
+			</MenuItem>
+		);
+		*/
 		return (
 			<div className={classnames("App", this.props.className)}>
 				<h3>Connecting shelters with the right patrons to fight against homelessness</h3>
 				<span>Organization looking to sign up? <a onClick={this.handleMove}>Register Now</a></span>
 				<h1>Open Requests</h1>
-				<ListGroup>
+				<span>Filter by:</span>
+				<ButtonGroup>
+					<Button onClick={this.handleDate}>by date</Button>
+					<DropdownButton title="by Organization.." id="bg-nested-dropdown">
+					</DropdownButton>
+				</ButtonGroup>
+				<ListGroup className="request-list">
 					{requests}
 				</ListGroup>
 			</div>
