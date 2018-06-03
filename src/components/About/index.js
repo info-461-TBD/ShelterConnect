ecb5758679d70f3528bb39c336df9b130b6b493d
Sunwoo Kang <s.kangsunwoo@gmail.com>
2018-06-03 04:06:24 -0700

    photos in about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import classnames from "classnames";
-
+import Joseph from "./Joseph.jpg";
+import Ryker from "./Ryker.jpg";
+import Simon from "./Simon.jpg";
+import Sunwoo from "./Sunwoo.jpg";
 import "./style.css";
 
 export default class About extends Component {
@@ -14,12 +17,34 @@ export default class About extends Component {
 					<p>Homelessness is a serious issue all across America. In a recent study, it was found that over half a million people are considered homeless in the United States. There are over <b>11,000</b> people that are homeless in Seattle alone, <b>47.1%</b> of which are suffering without shelter. We know that there are several organizations in our city that are working towards ending homelessness once and for all, but the issue will not disappear overnight. No matter how noble their mission may be, they still need help.</p>
 					<h1>Our Solution</h1>
 					<p>Our solution to combating homelessness in Seattle is to centralize the information that will help homeless shelters receive the proper donations they need. Through our website, organizations can post donation requests based on any specific events or drives that they are running. Some organizations are smaller and may not have the opportunity to advertise for help outside of their immediate circle of connections. But with this website, volunteers and donors can view these posts from any organization to see what items or resources that they can give to these causes. We simply want to connect those who need help with those who can offer it.
-					<br/>
-					<br/>
-	We believe that this can really help fight against homelessness. 
+					<br />
+						<br />
+						We believe that this can really help fight against homelessness.
 	We believe in the organizations that are working hard to provide resources for those in need. We believe in the people that want to help see better living conditions for all. And we believe in the common people of society, who can really make a difference by laying down what they have. We know that our solution will help connect all of these people to make society a better place.</p>
 				</div>
-			</div>
-		);
+				<section id="testimonials">
+					<div class="container">
+						<div class="testimonial">
+							<img src={Joseph} alt="profile photo" class="profile-img"/>
+								<h2>Joseph Zhang <span class="occupation">Developer</span></h2>
+								
+                		</div>
+						<div class="testimonial">
+							<img src={Ryker} alt="profile photo" class="profile-img"/>
+								<h2>Ryker Schwartzenberger <span class="occupation">Developer</span></h2>
+		                </div>
+						<div class="testimonial">
+							<img src={Simon} alt="profile photo" class="profile-img"/>
+								<h2>Simon Bang <span class="occupation">Developer</span></h2>
+		                </div>
+						<div class="testimonial">
+							<img src={Sunwoo }alt="profile photo" class="profile-img"/>
+								<h2>Sunwoo Kang <span class="occupation">Project Manager</span></h2>
+		                </div>
+					</div>
+		        </section>
+
+					</div>
+					);
 	}
 }
