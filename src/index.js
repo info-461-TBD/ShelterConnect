8ff7f63bba72143d50f3ae385c435b408bbe5fda
Ryker Schwartzenberger <rykerls@uw.edu>
2018-06-04 15:34:58 -0700

    adding gh-pages task and updated package.json

--- src/index.js
+++ src/index.js
@@ -10,15 +10,7 @@ const outlet = document.getElementById("root");
 
 
 if (firebase_helper.initialize()) {
-    //Signs in user rykerls@uw.edu
-    // if (firebase_helper.signInUser("rykerls@uw.edu", "test00")) {
-        
 
-    //console.log(firebase_helper.getRequestList());
-    //console.log(firebase_helper.getSingleRequest(1));
-
-    // Uncomment this line and increment the number after '+' in the first argument to create a new user.
-    // firebase_helper.createUser("rykerls+91@uw.edu", "test00", "test1", "",  "rykerls1@uw.edu", 55555555, "123 Easy st.", "test test test");
 }
 
-ReactDOM.render(<Router history={browserHistory} routes={Routes} />, outlet);
+ReactDOM.render(<Router history={hashHistory} routes={Routes} />, outlet);
