364303f5d2acb8222844b985d05ce3869f3bf5aa
Ryker Schwartzenberger <rykerls@uw.edu>
2018-06-04 15:37:01 -0700

    added hashHistory to imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, browserHistory } from "react-router";
+import { Router, hashHistory } from "react-router";
 import Routes from "./routes";
 import * as firebase_helper from "./firebase_helper.js";
 
